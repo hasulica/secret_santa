8f02d1683dad1bb26e1029239408de35a0f859e0
hasulica <raduasandei@gmail.com>
2015-11-18 18:32:40 +0000

    Persistent GitHub Auth

--- app.rb
+++ app.rb
@@ -1,39 +1,67 @@
-require 'sinatra/base'
+require 'sinatra'
+require 'rest_client'
 require 'json'
-require 'rest-client'
+
 
 CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
 CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
 
 class SecretSanta < Sinatra::Base
+
+  use Rack::Session::Pool, :cookie_only => false
+
+  def authenticated?
+    session[:access_token]
+  end
+
+  def authenticate!
+    erb :index, :locals => {:client_id => CLIENT_ID}
+  end
+
   get '/' do
-    erb :index, locals: { client_id: CLIENT_ID }
+    if !authenticated?
+      authenticate!
+    else
+      access_token = session[:access_token]
+      scopes = []
+
+      begin
+        auth_result = RestClient.get('https://api.github.com/user',
+                                     {:params => {:access_token => access_token},
+                                      :accept => :json})
+      rescue => e
+        session[:access_token] = nil
+        return authenticate!
+      end
+
+      if auth_result.headers.include? :x_oauth_scopes
+        scopes = auth_result.headers[:x_oauth_scopes].split(', ')
+      end
+
+      auth_result = JSON.parse(auth_result)
+
+      if scopes.include? 'user:email'
+        auth_result['private_emails'] =
+          JSON.parse(RestClient.get('https://api.github.com/user/emails',
+                         {:params => {:access_token => access_token},
+                          :accept => :json}))
+      end
+
+      erb :advanced, :locals => auth_result
+    end
   end
 
   get '/callback' do
     session_code = request.env['rack.request.query_hash']['code']
 
     result = RestClient.post('https://github.com/login/oauth/access_token',
-                             { client_id: CLIENT_ID,
-                               client_secret: CLIENT_SECRET,
-                               code: session_code },
-                             accept: :json)
+                            {:client_id => CLIENT_ID,
+                             :client_secret => CLIENT_SECRET,
+                             :code => session_code},
+                             :accept => :json)
 
-    access_token = JSON.parse(result)['access_token']
-    scopes = JSON.parse(result)['scope'].split(',')
-    has_user_email_scope = scopes.include? 'user:email'
-    auth_result =
-      JSON.parse(RestClient.get('https://api.github.com/user',
-                                            params: {access_token: access_token }))
-    if has_user_email_scope
-      auth_result['private_emails'] =
-        JSON.parse(RestClient.get('https://api.github.com/user/emails',
-                                  params: { access_token: access_token }))
-    end
+    session[:access_token] = JSON.parse(result)['access_token']
 
-    erb :basic, locals: auth_result
+    redirect '/'
   end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $PROGRAM_NAME
 end
